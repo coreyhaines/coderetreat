f14c36db751956b8e41ee5c57cdcc7415b36ee87
Adrian Mowat <adrian.mowat@gmail.com>
2011-11-11 06:52:29 +0000

    Added Clojure starting point

---
+++ starting_points/clojure/lein/conways/project.clj
@@ -0,0 +1,3 @@
+(defproject conways "1.0.0-SNAPSHOT"
+  :description "Conways Game of Life"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ starting_points/clojure/lein/conways/test/conways/test/core.clj
@@ -0,0 +1,6 @@
+(ns conways.test.core
+  (:use [conways.core])
+  (:use [clojure.test]))
+
+(deftest conways-test
+  (is false "Breath life into me!"))
