e2d50a5a12886d78b4af00baa34adf48a43bb25f
John Brewer <brewerja@gmail.com>
2013-07-19 21:07:24 -0400

    Added a Python starting point.

---
+++ starting_points/python/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from game_of_life import *
+
+
+class GameOfLifeTest(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
