bcfe0ac2d3b49985ca3e8fd4ccd777f57aff1f16
Akim Boyko <akim.boyko@gmail.com>
2013-12-12 18:35:04 +0100

    Add f# 3.0 vs2012 starting point with FsUnit/xUnit/FluentAssertions

---
+++ starting_points/fsharp/vs2012/GameOfLife.fs
@@ -0,0 +1,12 @@
+﻿namespace GameOfLife
+
+module GameOfLife =
+    open System
+    open System.Collections.Generic
+    open System.Linq
+    open System.IO
+    open System.Threading
+    open Microsoft.FSharp.Text
+    open Microsoft.FSharp.Linq
+
+    0
