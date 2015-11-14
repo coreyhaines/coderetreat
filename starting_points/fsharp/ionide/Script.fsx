d63516fe191c6420926fbb50c33b39ebaab2b752
Martin Gondermann <magicmonty@pagansoft.de>
2015-11-14 08:55:46 +0100

    Added F# starter for Atom ionide

---
+++ starting_points/fsharp/ionide/Script.fsx
@@ -0,0 +1,7 @@
+// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "ionide.fs"
+open ionide
+
+// Define your library scripting code here
